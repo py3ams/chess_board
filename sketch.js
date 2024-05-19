2add19ae063063ade25ec0d754f615347728bc62
Aaron Smith <aaronsmith540@gmail.com>
2024-05-19 16:03:12 +0200

    add header

--- sketch.js
+++ sketch.js
@@ -1,19 +1,11 @@
-//This is a simple template for you to experiment with
-
-//This is a comment  ... the two slashes at the start mean that the computer ignores it
-
-//You can look up more commands here ... https://p5js.org/reference/
 
 var left_speed = 1;
 var right_speed = 1;
 
 function setup()
 {
-    //this function runs once at the start of your program
-    
-    //this command creates a drawing area in the browser - it should only be called once
     createCanvas(600,600);
-    background(200,200,200)
+    background(255)
 
 }
 
